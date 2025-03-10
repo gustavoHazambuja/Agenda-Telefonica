6d7d2187a79ddb2120ed6903c9db137ea03c695e
Gustavo Henrique <guazambujinha@gmail.com>
2025-03-10 16:38:57 -0300

    fist commit

---
+++ Entites/Person.java
@@ -0,0 +1,35 @@
+package Entites;
+
+public class Person{
+
+    private String name;
+    private String number;
+
+    public Person(){}
+
+    public Person(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("\nNome: %s\n" + "Telefone: %s\n",name, number);
+    }
+}
